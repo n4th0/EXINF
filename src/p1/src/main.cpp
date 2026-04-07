02fca38d088ca34a34f6f232a9214869b913e460
n4th0 <nathanmoyses@gmail.com>
2026-04-07 10:50:27 +0200

    added files from evaluation

--- src/p1/src/main.cpp
+++ src/p1/src/main.cpp
@@ -2,24 +2,27 @@
 #include <iostream>
 #include <list>
 #include <string>
+#include <sys/resource.h>
 
 using namespace std;
 
-void imprimirListaSTL(const list<string> &cadena) {
-  list<string>::const_iterator itCadena;
-  for (itCadena = cadena.begin(); itCadena != cadena.end(); itCadena++) {
-    cout << (*itCadena) << ", ";
-  }
-  cout << endl;
+double getcputime(void) {
+  struct timeval tim;
+  struct rusage ru;
+  getrusage(RUSAGE_SELF, &ru);
+  tim = ru.ru_utime;
+  double t = (double)tim.tv_sec + (double)tim.tv_usec / 1000000.0;
+  tim = ru.ru_stime;
+  t += (double)tim.tv_sec + (double)tim.tv_usec / 1000000.0;
+  return t;
 }
 
-int main(void) {
-  bool kCasosEspeciales = true, kpasarAminusculas = true;
+int main() {
 
-  list<string> lt1, lt2, lt3;
+  long double aa = getcputime();
+  Tokenizador a("\t ,;:.-+/*_`'{}[]()!?&#\"\\<>", true, true);
+  a.TokenizarListaFicheros("./test/listaFicheros.txt"); // TODO EL CORPUS
+  cout << "Ha tardado " << getcputime() - aa << " segundos" << endl;
 
-  Tokenizador a("[]# ", kCasosEspeciales, kpasarAminusculas);
-
-  a.Tokenizar("äëïöü ÁÉÍÓÚ", lt1);
-  imprimirListaSTL(lt1);
+  return 0;
 }
